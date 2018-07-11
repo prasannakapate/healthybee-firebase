90ae6395dda4b7262273802c495bd2495df5967b
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-07-11 09:20:49 +0530

    CRUD Operation done

---
+++ functions/config/constants/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  END_POINTS: {
+    FACTS: '/facts',
+    MENU: '/menu',
+    CONTACTS: '/contacts'
+  },
+  COLLECTIONS: {
+    FACTS: 'facts',
+    MENU: 'menu',
+    CONTACTS: 'contacts'
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,73 +1,47 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase-admin');
 const express = require('express');
 const firebaseHelper = require('firebase-functions-helper');
 const app = express();
 const bodyParser = require('body-parser');
+const contactsApi = require('./api/contacts');
+const menuApi = require('./api/menu');
+const CONST = require('./config/constants');
+require('./config/db');
+
+app.use(CONST.END_POINTS.CONTACTS, contactsApi.contactsRouter);
+app.use(CONST.END_POINTS.MENU, menuApi.menuRouter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//configs
-const firebaseApp = firebase.initializeApp(functions.config().firebase);
-const db = firebase.firestore();
-
-function getFacts() {
-    const ref = firebaseApp.database().ref('facts');
-    return ref.once('value').then(snap => snap.val());
-}
-
 app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`);
+  response.send(`${Date.now()}`);
 });
 
 app.get('/timestamp-cached', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
-    response.send(`${Date.now()}`);
+  response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
+  response.send(`${Date.now()}`);
 });
 
-app.get('/getfacts', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-max-age=600');
-    getFacts()
-        .then(facts => {
-            return response.status(200).send({ facts });
-        })
-        .catch(error => {
-            console.log(error);
-        });
+app.post(CONST.END_POINTS.FACTS, (req, res) => {
+  firebaseHelper.firestore.creatNewDocument(db, CONST.COLLECTIONS.FACTS, req.body);
+  res.send('created successfully');
 });
 
-app.post('/facts', (req, res) => {
-    firebaseHelper.firestore
-        .creatNewDocument(db, 'facts', req.body);
-    res.send('created successfully');
+app.get(CONST.END_POINTS.FACTS, (req, res) => {
+  firebaseHelper.firestore
+    .backup(db, CONST.COLLECTIONS.FACTS)
+    .then(data => res.status(200).json(data))
+    .catch(error => console.log(error));
 });
 
-app.get('/facts', (req, res) => {
-    firebaseHelper.firestore
-        .backup(db, 'facts')
-        .then(data => res.status(200).json(data))
-        .catch(error => console.log(error));
+app.post(CONST.END_POINTS.FACTS, (req, res) => {
+  firebaseHelper.firestore.creatNewDocument(db, CONST.COLLECTIONS.FACTS, req.body);
+  res.send('created successfully');
 });
 
-app.post('/menu/:category', (req, res) => {
-    firebaseHelper.firestore
-        .creatNewDocument(db, 'menu', req.params.category, req.body);
-    res.send('created successfully');
-});
-
-app.get('/menu', (req, res) => {
-    firebaseHelper.firestore
-        .backup(db, 'menu')
-        .then(data => res.status(200).json(data))
-        .catch(error => console.log(error));
-});
-
-app.get('/menu/:category', (req, res) => {
-    firebaseHelper.firestore
-        .backup(db, 'menu', req.params.category)
-        .then(data => res.status(200).json(data))
-        .catch(error => console.log(error));
+app.get('*', (req, res) => {
+  res.status(404).json({ error: 'This route does not exist.' });
 });
 
 exports.api = functions.https.onRequest(app);
