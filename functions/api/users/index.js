64e5b2fe0899c48167c9269fdc18c396d00227ce
Prasannakumar Kapate <prasanna.1616@gmail.com>
2018-08-09 19:37:02 +0530

    code clean up

--- functions/api/users/index.js
+++ functions/api/users/index.js
@@ -1,24 +1,18 @@
 // USER api
 
 const express = require('express');
-const firebaseHelper = require('firebase-functions-helper');
 let usersRouter = express.Router();
-const CONST = require('../../config/constants');
+// const CONST = require('../../config/constants');
 require('../../config/db');
-
-//reference to the database collection
-// const usersCollection = db.collection("users");
 
 // Get USER api
 usersRouter.get('/:uid', (req, res) => {
   admin.auth().getUser(req.params.uid)
-    .then(function (userRecord) {
+    .then((userRecord) => {
       // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully fetched user data:");
-      res.status(200).json(userRecord);
+      return res.status(200).json(userRecord);
     })
-    .catch(function (error) {
-      console.log("Error fetching user data:", error);
+    .catch((error) => {
       res.status(404).json(error);
     });
 });
@@ -26,16 +20,14 @@ usersRouter.get('/:uid', (req, res) => {
 //Create new user
 usersRouter.post("/", (req, res, next) => {
   admin.auth().createUser(req.body)
-    .then(function (userRecord) {
+    .then((userRecord) => {
       // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully created new user:", userRecord.uid);
-      res.status(201).json({
+      return res.status(201).json({
         statusMessage: "ok",
         message: 'Created data successfully'
       });
     })
-    .catch(function (error) {
-      console.log("Error creating new user:", error);
+    .catch((error) => {
       res.status(404).json(error);
     });
 });
@@ -44,16 +36,14 @@ usersRouter.post("/", (req, res, next) => {
 // Update USER api
 usersRouter.patch('/:uid', (req, res) => {
   admin.auth().updateUser(req.params.uid, req.body)
-    .then(function (userRecord) {
+    .then((userRecord) => {
       // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully updated user");
-      res.status(200).json({
+      return res.status(200).json({
         statusMessage: "ok",
         message: 'Updated data successfully'
       });
     })
-    .catch(function (error) {
-      console.log("Error updating user:", error);
+    .catch((error) => {
       res.status(404).json(error);
     });
 });
@@ -61,15 +51,13 @@ usersRouter.patch('/:uid', (req, res) => {
 // Delete USER api
 usersRouter.delete('/:uid', (req, res) => {
   admin.auth().deleteUser(req.params.uid)
-    .then(function () {
-      console.log("Successfully deleted user");
-      res.status(200).json({
+    .then(() => {
+      return res.status(200).json({
         statusMessage: "ok",
         message: 'Record deleted Successfully'
       });
     })
-    .catch(function (error) {
-      console.log("Error deleting user:", error);
+    .catch((error) => {
       res.status(404).send(error);
     });
 });
